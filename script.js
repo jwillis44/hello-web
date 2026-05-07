4cacdf0b929f8f90143d77fab625f0a19d105679
jwillis <jwillis@m-ecs.org>
2026-05-07 11:52:42 -0400

    Add click counter with localStorage

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const themeButton = document.getElementById("themeButton"); //theme button
 const button = document.getElementById("helloButton"); //click me button
+const clickCountText = document.getElementById("clickCount"); //click counter text
 let clicked = false; //click me default
+let clickCount = Number(localStorage.getItem("clickCount")) || 0;
+clickCountText.textContent = "Click count: " + clickCount;
 // theme button
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark-mode");
   }
 
 button.addEventListener("click", function () {
+  clickCount += 1;
+  clickCountText.textContent = "Click count: " + clickCount;
+  localStorage.setItem("clickCount", clickCount);
+
   if (!clicked) {
     button.textContent = "You clicked me!";
     clicked = true;
